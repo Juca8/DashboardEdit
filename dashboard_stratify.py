60bbb6a1bc906b67b7ec9e6434922e707b90f7bc
uvuvwevwevwe onyetenyevwe ugwemuhwem osas <a00835982@tec.mx>
2025-06-12 09:56:28 -0600

    Update dashboard_stratify.py

--- dashboard_stratify.py
+++ dashboard_stratify.py
@@ -71,7 +71,7 @@ st.markdown("""
     [data-testid="stSidebar"] .stSelectbox,
     [data-testid="stSidebar"] .stRadio {
         border-radius: 8px;
-        color: black !important;
+        color: wite !important;
     }
 
      .titulo-container {
@@ -97,6 +97,24 @@ modelo = load_model()
 df = load_data()
 
 # ========== SIDEBAR CLUSTER (Texto blanco, grande y en negritas) ==========
+st.markdown("""
+    <style>
+    /* Texto dentro del campo seleccionado del selectbox */
+    .stSelectbox div[data-baseweb="select"] > div {
+        color: black !important;
+    }
+
+    /* Texto de la opción seleccionada */
+    .stSelectbox span {
+        color: black !important;
+    }
+
+    /* Fondo blanco del cuadro y del dropdown */
+    .stSelectbox div[data-baseweb="select"] {
+        background-color: white !important;
+    }
+    </style>
+""", unsafe_allow_html=True)
 st.sidebar.markdown("<h2 style='color:white; font-size:24px; font-weight:bold;'>Filtros</h2>", unsafe_allow_html=True)
 
 cluster_names = {
